c6f5c1287beb28863cb0ba2b7ed6e14403d0cd12
Cristian <cristian@Criss-MacBook-Pro.local>
2020-07-04 15:09:51 -0700

    Completed 4/5 JS & DS Certification 1 more to go

---
+++ telephone-number-validator.js
@@ -0,0 +1,59 @@
+function telephoneCheck(str) {
+  let container = [], isValid = false
+  let counter = 0, symbolCount = 0
+
+  //removing nonalphanumeric characters
+  for(var i = 0; i < str.length;i++){
+    if(str[i] != '-' && str[i] != '(' && str[i] != ')' && str[i] != '*' && str[i] != '&' && str[i] != '!' && str[i] != '#' && str[i] != '?' && str[i] != ' '){
+        container.push(str[i])
+    }
+    else if(str[i] == ')'){
+      counter++
+    }
+    else if(str[i] == '('){
+      counter++
+    }
+    else if(str[i] == '?'){
+      symbolCount++
+    }
+    else if(str[0] == '-'){
+      return isValid
+    }
+  }
+  const containerLen = container.length
+
+  if(containerLen <= 8){
+    isValid = false
+  }
+  else if(containerLen == 11 || containerLen == 10){
+    
+    let key = parseInt(container[0])
+    switch(key){
+      case 2:
+      isValid = false
+      break
+      case 5:
+      isValid = true
+      break
+      case 1:
+      isValid = true
+      break
+      case -1:
+      isValid = false
+      break
+      case 0:
+      isValid = false
+      break
+    }
+  }
+
+  if(counter == 1){
+    isValid = false
+  }
+  else if(symbolCount == 1){
+    isValid = false
+  }
+  return isValid;
+}
+
+telephoneCheck("555-555-5555");
